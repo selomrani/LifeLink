76b682760b709669b4a09e198873a10e0ffbe3cb
Soufyan <elomranisoufyan@gmail.com>
2026-04-16 11:34:10 +0100

    finished blood request post crud and comments management

--- app/Http/Controllers/Feed/BloodRequestController.php
+++ app/Http/Controllers/Feed/BloodRequestController.php
@@ -3,9 +3,65 @@
 namespace App\Http\Controllers\Feed;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\BloodPostRequest;
+use App\Models\BloodREquestPost;
 
 class BloodRequestController extends Controller
 {
-    //
+    public function index()
+    {
+        $allbloodrequests = BloodREquestPost::with('comments')->get();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $allbloodrequests,
+            'message' => 'All Blood Requests',
+        ]);
+    }
+
+    public function show(BloodREquestPost $bloodrequest)
+    {
+        return response()->json([
+            'status' => 'success',
+            'data' => $bloodrequest,
+            'message' => 'Blood Request',
+            'comments' => $bloodrequest->comments,
+        ]);
+    }
+
+    public function store(BloodREquestPost $request)
+    {
+        $validated = $request->validated();
+
+        $bloodrequest = BloodREquestPost::create($validated);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $bloodrequest,
+            'message' => 'Blood Request created successfully',
+        ]);
+    }
+
+    public function destroy(BloodREquestPost $bloodrequest)
+    {
+        $bloodrequest->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Blood Request deleted successfully',
+        ]);
+    }
+
+    public function update(BloodREquestPost $bloodrequest, BloodPostRequest $request)
+    {
+        $validated = $request->validated();
+
+        $bloodrequest->update($validated);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $bloodrequest,
+            'message' => 'Blood Request updated successfully',
+        ]);
+    }
 }
